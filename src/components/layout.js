351bfa422797b60c089aafb3bef43ccf8f9f60de
msubhasish1601 <56871360+msubhasish1601@users.noreply.github.com>
2020-02-24 19:11:44 +0530

    indutry change

--- src/components/layout.js
+++ src/components/layout.js
@@ -66,6 +66,9 @@ const TemplateWrapper = ({ children }) => {
                 <li>
                   <Link to="/about">About</Link>
                 </li>
+                <li>
+                  <Link to="/industry">Industry</Link>
+                </li>
               </ul>
               <p className="sidebar__social">
                 {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
